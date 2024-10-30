7122c029b65ba2872d1638729d8ba750296bee6e
Alvin <gabriel.alvin7@gmail.com>
2024-10-30 13:42:27 -0400

    Update

---
+++ Aula1PHP32/variaveis.php
@@ -0,0 +1,10 @@
+<?php
+    $nome = "NOME";
+    $sobrenome = "SOBRENOME";
+    $idade = "IDADE";
+    $profissao = "PROFISSAO";
+
+
+    echo "OI $profissao $nome $sobrenome. <br>";
+    echo "Voce possui atualmente $idade anos."
+?>
